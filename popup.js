f7ef3af4ded0020684f19f103c54f0caef7171f2
Trent Slutzky <trent.slutzky@gmail.com>
2023-04-06 01:33:48 -0400

    add popup to enable/disable

---
+++ popup.js
@@ -0,0 +1,43 @@
+console.log("HELLO FROM POPUP");
+
+button_color_disabled = "#a03434";
+button_color_enabled = "#629562";
+
+button = document.getElementById("enable-button");
+reload_required_text = document.getElementById("reload-required");
+button.style.opacity = 0;
+
+chrome.storage.local.get(["state"]).then((res) => {
+  new_state = "enabled";
+  if(res.state){
+    new_state = res.state;
+  }
+  button.innerHTML = new_state;
+  if(new_state != "enabled"){
+    button.style.background = button_color_disabled;
+  } else {
+    button.style.background = button_color_enabled;
+  }
+  button.style.opacity = 1;
+})
+
+button.onclick = async function (e) {
+  chrome.storage.local.get(["state"]).then((res) => {
+    new_state = "enabled";
+    if(res.state){
+      if(res.state == "enabled"){
+        new_state = "disabled";
+      }
+    }
+    chrome.storage.local.set({state: new_state}).then(() => {
+      reload_required_text.style.opacity = 1;
+      button.innerHTML = new_state;
+      if(new_state != "enabled"){
+        button.style.background = button_color_disabled;
+      } else {
+        button.style.background = button_color_enabled;
+      }
+    })
+  })
+}
+
--- threadhider.js
+++ threadhider.js
@@ -1,4 +1,3 @@
-console.log("HSTH Loaded.");
 
 const observer = new MutationObserver(function(mutationList, observer) {
   injectButtons();
@@ -8,9 +7,18 @@ options = { attributes: false, childList: true, subtree: true };
 observer.observe(document, options);
 
 function injectButtons(){
-  refreshThreads();
-  articles = document.querySelectorAll("article.thread.customer");
-  articles.forEach(article => addButtonToCustomerThread(article));
+  chrome.storage.local.get(["state"]).then((res) => {
+    new_state = "enabled";
+    if(res.state){
+      new_state = res.state;
+    }
+    console.log(new_state);
+    if(new_state == "enabled"){
+      refreshThreads();
+      articles = document.querySelectorAll("article.thread.customer");
+      articles.forEach(article => addButtonToCustomerThread(article));
+    }
+  });
 }
 
 function refreshThreads(){
@@ -20,10 +28,12 @@ function refreshThreads(){
       let thread_id_string = article.id;
       let thread_id = thread_id_string.split("-")[1];
       let body_section = article.querySelector("section.js-thread-content > section.messageBodyWrap > section.message > section.messageBody");
-      if(res.hsth_hidden_threads.includes(thread_id)){
-        body_section.style.display = "none";
-      } else {
-        body_section.style.display = "block";
+      if(res.hsth_hidden_threads){
+        if(res.hsth_hidden_threads.includes(thread_id)){
+          body_section.style.display = "none";
+        } else {
+          body_section.style.display = "block";
+        }
       }
     });
   })
@@ -46,9 +56,15 @@ function addButtonToCustomerThread(article) {
     btn.id = thread_id+"-button";
 
     let hide = false;
-    if(res.hsth_hidden_threads.includes(thread_id)){
-      btn.innerHTML = "hidden";
-      btn.setAttribute("show", true);
+    if(res.hsth_hidden_threads){
+      if(res.hsth_hidden_threads.includes(thread_id)){
+        btn.innerHTML = "hidden";
+        btn.setAttribute("show", true);
+      } else {
+        hide = true;
+        btn.innerHTML = "hide";
+        btn.setAttribute("show", false);
+      }
     } else {
       hide = true;
       btn.innerHTML = "hide";
@@ -63,14 +79,20 @@ function addButtonToCustomerThread(article) {
         if(button.getAttribute("show") == "false"){
           button.setAttribute("show", true);
           button.innerHTML = "hidden";
-          current.push(thread_id);
-          result = current;
+          if(current){
+            current.push(thread_id);
+            result = current;
+          } else {
+            result = [thread_id];
+          }
         } else {
           button.setAttribute("show", false);
           button.innerHTML = "hide";
-          result = current.filter(function(x) {
-            return x !== thread_id;
-          })
+          if(current){
+            result = current.filter(function(x) {
+              return x !== thread_id;
+            })
+          }
         }
         chrome.storage.local.set({hsth_hidden_threads: result}).then(() => {
           refreshThreads();
@@ -85,4 +107,3 @@ function addButtonToCustomerThread(article) {
   })
 }
 
-injectButtons();
